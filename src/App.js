b52ab57cdc0e0994b3113c35830479aa92306b42
kazufoot21 <kazufoot21@outlook.jp>
2021-12-28 11:05:19 +0900

    styled

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import './App.css';
-
 function App() {
   return (
     <>
-      <h1>To Do List</h1>
+      <div className="App">
+        <h1>To Do List</h1>
 
-      <form>
-        <label autocomplete="off">
-          <input
-            type="text"
-            name="todos"
-            id="todos"
-            placeholder="Add Your To Do List"
-          />
-        </label>
-        <button type="submit">Create</button>
-      </form>
-
-      <ul>
-        <li>
-          <label htmlFor className>
-            <input type="checkbox" id />
+        <form>
+          <label autocomplete="off">
+            <input
+              type="text"
+              name="todos"
+              id="todos"
+              placeholder="Add Your To Do List"
+            />
           </label>
-          <button>edit</button>
-        </li>
-        <li>
-          <label htmlFor className>
-            <input type="checkbox" id />
-          </label>
-          <button>edit</button>
-        </li>
-      </ul>
+          <button type="submit">Create</button>
+        </form>
 
-      <div className="row">
-        <label htmlFor="all" name="all" id="all">
-          <input type="checkbox" />
-          All
-        </label>
-        <p>last To dos 0</p>
-        <button id="delete">delete</button>
+        <ul>
+          <li>
+            <label htmlFor className>
+              <input type="checkbox" id />
+              reading
+            </label>
+            <button>edit</button>
+          </li>
+          <li>
+            <label htmlFor className>
+              <input type="checkbox" id />
+              workout
+            </label>
+            <button>Edit</button>
+          </li>
+        </ul>
+
+        <div className="row">
+          <label htmlFor="all" name="all" id="all">
+            <input type="checkbox" />
+            All
+          </label>
+          <p>last To dos 0</p>
+          <button id="delete">Delete</button>
+        </div>
       </div>
     </>
   );
