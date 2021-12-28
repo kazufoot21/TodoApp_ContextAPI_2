9d7990f84a003edb3101ffda56e1fed821bb3e8d
kazufoot21 <kazufoot21@outlook.jp>
2021-12-28 14:20:01 +0900

    create delete function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,51 @@
 import React, { useContext } from 'react';
+import { useState } from 'react/cjs/react.development';
 import { DataContext } from './DataProvider';
 
 export const Footer = () => {
   const [todos, setTodos] = useContext(DataContext);
+  const [checkAll, setCheckAll] = useState(false);
+
+  const handleCheckAll = () => {
+    const newTodos = [...todos];
+    newTodos.forEach((todo) => {
+      todo.complete = !checkAll;
+    });
+    setTodos(newTodos);
+    setCheckAll(!checkAll);
+  };
+
+  const newTodoComplete = () => {
+    return todos.filter((todo) => {
+      return todo.complete === false;
+    });
+  };
+
+  const deleteTodo = () => {
+    setTodos(newTodoComplete());
+    setCheckAll(false);
+  };
+
   return (
-    <div className="row">
-      <label htmlFor="all" name="all" id="all">
-        <input type="checkbox" />
-        All
-      </label>
-      <p>last To dos 0</p>
-      <button id="delete">Delete</button>
-    </div>
+    <>
+      {todos.length === 0 ? (
+        <h2>GOOD JOB!!</h2>
+      ) : (
+        <div className="row">
+          <label htmlFor="all" name="all" id="all">
+            <input
+              type="checkbox"
+              onChange={handleCheckAll}
+              checked={checkAll}
+            />
+            All
+          </label>
+          <p id="count">You have {newTodoComplete().length} to do </p>
+          <button id="delete" onClick={deleteTodo}>
+            Delete
+          </button>
+        </div>
+      )}
+    </>
   );
 };
